83e5a8f9b558672c845f340b74609af429aa22a3
SergeyMNet <sergeym.net@gmail.com>
2018-01-05 14:01:54 +0200

    refactor firebase

---
+++ src/environments/firebase.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyACBslO6f4CubvwnzqjiZhTQK8hKxJImS4',
+    authDomain: 'alice-1d9df.firebaseapp.com',
+    databaseURL: 'https://alice-1d9df.firebaseio.com',
+    storageBucket: 'alice-1d9df.appspot.com',
+    messagingSenderId: '799235809725'
+  };
