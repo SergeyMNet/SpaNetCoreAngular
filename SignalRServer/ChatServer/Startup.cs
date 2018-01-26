6ca5cf3ea5525cc035f2ca31e0b97b633b52bd28
SergeyMNet <sergeym.net@gmail.com>
2018-01-26 18:12:07 +0200

    add dotnet core server

---
+++ SignalRServer/ChatServer/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ChatServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins("http://localhost:4200");
+            }));
+
+            services.AddMvc();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<TestHub>("test");
+            });
+        }
+    }
+}
---
+++ SignalRServer/ChatServer/TestHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServer
+{
+    public class TestHub : Hub
+    {
+        public Task IncrementCounter()
+        {
+
+            List<String> ConnectionIDToIgnore = new List<String>();
+            ConnectionIDToIgnore.Add(Context.ConnectionId);
+            return Clients.AllExcept(ConnectionIDToIgnore).InvokeAsync("IncrementCounter");
+        }
+
+        public Task DecrementCounter()
+        {
+            List<String> ConnectionIDToIgnore = new List<String>();
+            ConnectionIDToIgnore.Add(Context.ConnectionId);
+            return Clients.AllExcept(ConnectionIDToIgnore).InvokeAsync("DecrementCounter");
+        }
+
+
+        // chat
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.InvokeAsync("sendToAll", name, message);
+        }
+    }
+}
